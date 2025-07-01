13ca9f7026ebcbbee20adb275b5d371cdaf2490c
aayushxadhikari <aayushxadhikari@gmail.com>
2025-07-01 09:13:33 +0545

    completed the main

--- todo-cli/main.go
+++ todo-cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main(){
@@ -12,8 +13,10 @@ func main(){
 		fmt.Println("Usage: todo [add|list|done|delete]")
 		return 
 	}
+	// extracting the command 
 	cmd := os.Args[1]
 
+	// sending the data to add the task 
 	switch cmd{
 	case "add":
 		if len(os.Args)<3{
@@ -26,10 +29,12 @@ func main(){
 		}else{
 			fmt.Println("Task Added.")
 		}
+	// listing all teh list that have been added	
 	case "list":
 		if err := listTask(); err!=nil{
 			fmt.Println("Error:", err)
 		}
+	// converts the task id to integer and marks it as "done"
 	case "done":
 		if len(os.Args)<3{
 			fmt.Println("Usage: todo done <task id>")
@@ -45,7 +50,21 @@ func main(){
 		}else{
 			fmt.Println("Marked as Done!")
 		}
-	
-
+	case "delete":
+		if len(os.Args)<3{
+			fmt.Println("Usage: todo delete <task id>")
+		}
+		id, err:= strconv.Atoi(os.Args[2])
+		if err != nil{
+			fmt.Println("Invalid task ID")
+			return
+		}
+		if err := deleteTask(id); err!= nil{
+			fmt.Println("Error:", err)
+		} else{
+			fmt.Println("Task deleted")
+		}
+	default:
+		fmt.Println("Unknown command:", cmd)
 	}
 }
