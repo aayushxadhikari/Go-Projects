430fa584cd4443fcbeb19dbbb89426281458aec1
aayushxadhikari <aayushxadhikari@gmail.com>
2025-08-20 21:22:20 +0545

    complete the CLI code

--- learning-go/go-cli-crud-api/cli.go
+++ learning-go/go-cli-crud-api/cli.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+
 )
 
 func runCLI(args []string) {
@@ -11,8 +12,51 @@ func runCLI(args []string) {
 		return
 	}
 
-	switch args[0]{
+	switch args[0] {
 	case "create":
-		createCMD := flag.NewFlagSet("create", flag.ExitOnError)
+		createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+		name := createCmd.String("name", "", "Item name")
+		price := createCmd.Int("price", 0, "Item price")
+		createCmd.Parse(args[1:])
+		item := createItem(*name, *price)
+		printJSON(item)
+
+	case "list":
+		printJSON(listItems())
+
+	case "get":
+		getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+		id := getCmd.Int("id", 0, "Item ID")
+		getCmd.Parse(args[1:])
+		if item, ok := getItem(*id); ok {
+			printJSON(item)
+		} else {
+			fmt.Println("Item not found")
+		}
+
+	case "update":
+		updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+		id := updateCmd.Int("id", 0, "Item ID")
+		name := updateCmd.String("name", "", "New name")
+		price := updateCmd.Int("price", 0, "New price")
+		updateCmd.Parse(args[1:])
+		if item, ok := updateItem(*id, *name, *price); ok {
+			printJSON(item)
+		} else {
+			fmt.Println("Item not found")
+		}
+
+	case "delete":
+		deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+		id := deleteCmd.Int("id", 0, "Item ID")
+		deleteCmd.Parse(args[1:])
+		if ok := deleteItem(*id); ok {
+			fmt.Println("Item deleted:", *id)
+		} else {
+			fmt.Println("Item not found")
+		}
+
+	default:
+		fmt.Println("Unknown CLI command:", args[0])
 	}
 }
